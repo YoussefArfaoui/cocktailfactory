f970433f864079fb383f87a332c262116cf89057
YoussefArfaoui <arfafreeyouss@gmail.com>
2016-09-18 10:21:32 +0200

    create the project template

---
+++ src/main/java/com/cocktail/SampleController.java
@@ -0,0 +1,18 @@
+package com.cocktail;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+}
